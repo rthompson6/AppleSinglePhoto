00211d415fd1ada983d5fa869cfada08f1cbdde4
rthompson6 <32395237+rthompson6@users.noreply.github.com>
2017-10-11 14:33:40 -0600

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SinglePhoto
+//
+//  Created by ryan thompson on 2017-10-02.
+//  Copyright © 2017 ryan thompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
